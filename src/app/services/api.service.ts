4f6f63d93c36502bc6f3ccf5bf99799c88ad5291
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-09-22 13:34:56 +0300

    update project api  service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
-expect class apiService {
-    
+export class apiService {
+    constructor(private httpclient:HttpClient) {}
+    getprojects():Observable<any> {
+        return this.httpclient.get("https://nick-awwards.herokuapp.com/api/projects")
+
+    }
+
 }
